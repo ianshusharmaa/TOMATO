95920357714b869d1e75e62d491466d9785e70f3
Anshu Sharma <anshusharma4165@gmail.com>
2025-11-23 20:11:55 +0530

    user pannel logo

---
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets/frontend_assets/assets'
+
+
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="" srcset="" />
+        </div>
+    )
+}
+
+export default Navbar
